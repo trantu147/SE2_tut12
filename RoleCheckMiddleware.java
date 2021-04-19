318dca33256232c7c9ae13cedeeb8656ca681fa7
trantu193 <1801040193@s.hanu.edu.vn>
2021-04-19 19:48:01 +0700

    for tutorial purpose

---
+++ RoleCheckMiddleware.java
@@ -0,0 +1,18 @@
+package to_do.chain_of_responsibility.middleware;
+
+/**
+ * ConcreteHandler. Checks a user's role.
+ */
+public class RoleCheckMiddleware extends Middleware {
+	//TO-DO: Implement the check() method
+    public boolean check(String email, String password) {
+    	/*if email = 'admin@example.com' => display welcome message for admin then return true
+        else, display welcome message for normal user */
+        if(email.equals("admin@example.com")) {
+        	System.out.println("Welcome, admin!");
+        	return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
